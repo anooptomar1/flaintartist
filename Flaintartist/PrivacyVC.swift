1301276c7e0b37c6c2a82d2462a3153209f53319
Kerby Jean <kerby.jean@hotmail.fr>
2017-01-06 23:04:22 -0500

    report

--- Flaintartist/PrivacyVC.swift
+++ Flaintartist/PrivacyVC.swift
@@ -11,14 +11,13 @@ import UIKit
 class PrivacyVC: UIViewController {
 
     @IBOutlet var webView: UIWebView!
-    var urlToLoad: URL!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        webView.loadRequest(URLRequest(url: urlToLoad))
+        webView.loadRequest(URLRequest(url: URL(string: "//https://flaint.wordpress.com/2016/12/17/test/")!))
     }
 }
 
-//https://flaint.wordpress.com/2016/12/17/test/"
+
--- ProfileVC.swift
+++ ProfileVC.swift
@@ -339,14 +339,4 @@ class ProfileVC: UIViewController, iCarouselDelegate, iCarouselDataSource, UIIma
             })
         }
     }
-    
-    
-    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
-        
-        let webViewController = PrivacyVC()
-        webViewController.urlToLoad = URL
-        print("URL:\(URL)")
-        navigationController?.pushViewController(webViewController, animated: true)
-        return false
-    }
 }
